f42c946581b8af577b0d10936a85b5737d929802
Sneh Gour <Sneh Gour@inindcrws.com>
2024-08-27 11:45:35 +0530

    Initial1

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const msConfig = {
     auth: {
-        clientId: '401c2be7-f56c-4d65-a44c-f36d719b48f8',
-        authority: 'https://login.microsoftonline.com/1fea8d78-f353-4b2e-b145-3b2f5f1f0649',
+        clientId: 'ClientId',
+        authority: 'https://login.microsoftonline.com/TenantId',
         redirectUri: '/',
         postLogoutRedirectUri: '/',
         navigateToLoginRequestUrl: false
